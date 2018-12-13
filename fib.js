84d182e22b3205985e56402353543e472048630e
Luke Schuyler <lukeschuyler1@gmail.com>
2018-12-12 23:00:57 -0800

    converted to es6 class

---
+++ fib.js
@@ -0,0 +1,20 @@
+let fibArray = [1, 1];
+let args = process.argv;
+
+function fibonacci(num, memo) {
+  memo = memo || {};
+
+  if (memo[num]) return memo[num];
+  if (num <= 1) return 1;
+  
+  return memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo);
+}
+
+(function (num) {
+  for (let i = 1; i <= num; i++) {
+    fibArray.push(fibArray[fibArray.length - 1] + fibArray[fibArray.length - 2]);
+  }
+console.log("SEQUENCE: " + fibArray);
+console.log("Desired number is " + fibArray[num]);
+}(args[2]));
+console.log("THIS IS THE 1st ", fibonacci(process.argv[2]));
--- memo.js
+++ memo.js
@@ -1,45 +1,51 @@
-let numbs = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
+class Memo {
+  constructor() {
+    this.numbs = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
+    this.run();
+  }
 
-const memoize = fn => {
-  let cache = {};
-  return async (...args) => {
-    let n = args[0];
-    if (n in cache) {
-      console.log('Fetching from cache', n);
-      return await wait(cache[n]); // short(er) pause for cooler effect
-    }
-    else {
-      console.log('Not fetching from cache', n);
-      let result = await fn(n);
-      cache[n] = result;
-      return result; // mimic data call or some long process
+  memoize(fn) {
+    let cache = {};
+    return async (...args) => {
+      let n = args[0];
+      if (n in cache) {
+        console.log('Fetching from cache', n);
+        return await this.wait(cache[n]); // short(er) pause for cooler effect
+      }
+      else {
+        console.log('Not fetching from cache', n);
+        let result = await fn(n);
+        cache[n] = result;
+        return result; // mimic data call or some long process
+      }
     }
   }
-}
 
-const wait = n => {
-  return new Promise((res, rej) => {
-    setTimeout(function() {
-      res(n + 10);
-    }, 500);
-  });
-}
-
-
-const add = n => {
-  return new Promise((res, rej) => {
-    setTimeout(function() {
-      res(n);
-    }, 2000);
-  });
-}
-
-const memoized = memoize(add);
-
-
-let arr = new Array(200);
-(async function() {
-  for (let i of arr) {
-    await memoized(numbs[Math.floor(Math.random() * numbs.length)]);
+  wait(n) {
+    return new Promise((res, rej) => {
+      setTimeout(function() {
+        res(n + 10);
+      }, 500);
+    });
   }
-}());
+
+
+  add(n) {
+    return new Promise((res, rej) => {
+      setTimeout(function() {
+        res(n);
+      }, 2000);
+    });
+  }
+  
+  async run() {
+    const memoized = this.memoize(this.add);
+    let arr = new Array(200); // Needed iterable for async, used arbitrary array with length of 200 
+
+    for (let i of arr) {
+      await memoized(this.numbs[Math.floor(Math.random() * this.numbs.length)]);
+    }
+  };
+}
+
+new Memo();
