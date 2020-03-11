61cf75026b7231e5576dedee66febc96b02d8cae
Luke Schuyler <lukeschuyler1@gmail.com>
2020-03-10 21:50:27 -0700

    Added ts to check whether or not to grab from cache

--- memo.js
+++ memo.js
@@ -4,26 +4,40 @@ class Memo {
     this.run();
   }
 
+  async mimicFetch(n, cache, fn) {
+    console.log('Not fetching from cache', n);
+    let result = await fn(n);
+    cache[n] = { result, ts: Date.now() };
+    return result; // mimic data call or some long process
+  }
+
   memoize(fn) {
     let cache = {};
     return async (...args) => {
       let n = args[0];
       if (n in cache) {
-        console.log('Fetching from cache', n);
-        return await this.sleep(cache[n]); // short(er) pause for cooler effect
+        // If less than 10 seconds since cached, show as cached
+        let timeSincePrevious = Date.now() - cache[n].ts
+        console.log('\n')
+        console.log(timeSincePrevious)
+        console.log('since ' + n + ' was last fetched')
+        console.log('\n')
+        if (timeSincePrevious < 10000) {
+          console.log('Fetching from cache', n);
+          return await this.sleep(cache[n].result); // short(er) pause for effect
+        } else {
+          return this.mimicFetch(n, cache, fn)
+        }
       }
       else {
-        console.log('Not fetching from cache', n);
-        let result = await fn(n);
-        cache[n] = result;
-        return result; // mimic data call or some long process
+        return this.mimicFetch(n, cache, fn)
       }
     }
   }
 
   // Synthetic `sleep` funciton
   sleep(n) {
-    return new Promise((res, rej) => {
+    return new Promise(res => {
       setTimeout(function() {
         res(n + 10);
       }, 100);
@@ -31,7 +45,7 @@ class Memo {
   }
 
   add(n) {
-    return new Promise((res, rej) => {
+    return new Promise(res => {
       setTimeout(function() {
         res(n);
       }, 2000);
@@ -40,7 +54,10 @@ class Memo {
   
   async run() {
     const memoized = this.memoize(this.add);
-    let arr = new Array(200); // Needed iterable for async, used arbitrary array with length of 200 
+
+    // Needed iterable for async, used
+    // arbitrary array with length of 200 
+    let arr = new Array(200);
     
     for (let i of arr) {
       let random = Math.floor(Math.random() * this.numbs.length)
