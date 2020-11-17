46f74c25c13e2dbcb60f39f5bddbff55dc3c4fba
Luke Schuyler <lukeschuyler1@gmail.com>
2020-11-17 13:17:20 -0800

    Converted to typescript

--- memo.js
+++ memo.js
@@ -1,27 +1,33 @@
+type MemoCache = {
+  [index: string] : {
+    result: number, ts: number
+  }
+}
+
 class Memo {
-  constructor() {
-    this.numbs = [...Array(25).keys()].map(n => n + 1);
+  numbs: Array<number>;
+
+  constructor (numbs: Array<number>) {
+    this.numbs = numbs;
     this.run();
   }
 
-  async mimicFetch(n, cache, fn) {
+  async mimicFetch (n: number, cache: MemoCache = {}, fn: Function): Promise<number> {
     console.log('Not fetching from cache', n);
     let result = await fn(n);
     cache[n] = { result, ts: Date.now() };
     return result; // mimic data call or some long process
   }
 
-  memoize(fn) {
-    let cache = {};
-    return async (...args) => {
+  memoize (fn: Function) {
+    let cache: MemoCache = {};
+    return async (...args: Array<any>) => {
       let n = args[0];
       if (n in cache) {
         // If less than 10 seconds since cached, show as cached
         let timeSincePrevious = Date.now() - cache[n].ts
-        console.log('\n')
-        console.log(timeSincePrevious)
-        console.log('since ' + n + ' was last fetched')
-        console.log('\n')
+        this.logCacheStatus(timeSincePrevious, n);
+
         if (timeSincePrevious < 10000) {
           console.log('Fetching from cache', n);
           return await this.sleep(cache[n].result); // short(er) pause for effect
@@ -35,8 +41,15 @@ class Memo {
     }
   }
 
+  logCacheStatus (timeSincePrevious: number, n: number): void {
+    console.log('\n')
+    console.log(timeSincePrevious)
+    console.log('since ' + n + ' was last fetched')
+    console.log('\n')
+  }
+
   // Synthetic `sleep` funciton
-  sleep(n) {
+  sleep (n: number): Promise<number> {
     return new Promise(res => {
       setTimeout(function() {
         res(n + 10);
@@ -44,7 +57,7 @@ class Memo {
     });
   }
 
-  add(n) {
+  add (n: number): Promise<number> {
     return new Promise(res => {
       setTimeout(function() {
         res(n);
@@ -52,7 +65,7 @@ class Memo {
     });
   }
   
-  async run() {
+  async run (): Promise<void> {
     const memoized = this.memoize(this.add);
 
     // Needed iterable for async, used
@@ -66,4 +79,6 @@ class Memo {
   };
 }
 
-new Memo();
+new Memo(
+  [...Array(25).keys()].map(n => n + 1)
+);
